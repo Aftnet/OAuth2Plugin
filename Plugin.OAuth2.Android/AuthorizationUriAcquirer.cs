de55a25ddba84c555616cdd6be2710e034cd4aed
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-10-25 16:35:47 +0900

    Android stub

---
+++ Plugin.OAuth2.Android/AuthorizationUriAcquirer.cs
@@ -0,0 +1,14 @@
+﻿using Plugin.OAuth2.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace Plugin.OAuth2.Components
+{
+    public class AuthorizationUriAcquirer : IAuthorizationUriAcquirer
+    {
+        public Task<string> GetAuthorizationUriAsync(string authorizeUri, string redirectUriRoot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
