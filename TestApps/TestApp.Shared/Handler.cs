a241e01b0700801c6e3abfaf2b5ebc41bc4a690f
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-11-21 15:12:04 +0900

    Logic

---
+++ TestApps/TestApp.Shared/Handler.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.OAuth2;
+using Plugin.OAuth2.Common;
+using System.Threading.Tasks;
+
+namespace TestApp.Shared
+{
+    public class Handler
+    {
+        private readonly IOAuth2Broker Broker = CrossOAuth2.GetGithubBroker("", "", "");
+
+        public async Task<string> LogIn()
+        {
+            var output = await Broker.GetAccessTokenAsync();
+            return output;
+        }
+    }
+}
