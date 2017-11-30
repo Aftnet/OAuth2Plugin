c571a118833ecb3e5760f56ae422bcb176bc2ccd
claudiuslollarius <claudiuslollarius@gmail.com>
2017-11-30 17:03:52 +0900

    logic

---
+++ Plugin.OAuth2.macOS/AuthorizationUriAcquirer.cs
@@ -0,0 +1,18 @@
+﻿using Plugin.OAuth2.Common;
+using System.Threading.Tasks;
+
+namespace Plugin.OAuth2.Components
+{
+    internal class AuthorizationUriAcquirer : AuthorizationUriAcquirerBase
+    {
+        protected override Task ShowModalBrowserUI()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override Task CloseModalBrowserUI()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
