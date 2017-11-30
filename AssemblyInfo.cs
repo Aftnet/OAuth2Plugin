96e405c22af2681ff0a1e681c526ba13d4bba6a5
Alberto Fustinoni <alberto@aftnet.net>
2017-11-30 21:50:12 +0900

    Development

---
+++ AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alberto Fustinoni")]
+[assembly: AssemblyProduct("OAuth2 Xamarin Plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ TestApps/TestApp.macOS/ViewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace TestApp.macOS
+{
+    public partial class ViewController : NSViewController
+    {
+        private readonly Shared.Handler Handler = new Shared.Handler();
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void LogInBtnClick(NSObject sender)
+        {
+            var task = Handler.LogIn();
+        }
+    }
+}
