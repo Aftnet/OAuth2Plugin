eb89a338603c476cc0a44a343aac54c4a4cd04f8
claudiuslollarius <claudiuslollarius@gmail.com>
2017-12-01 13:37:37 +0900

    Logic

--- Plugin.OAuth2.macOS/AuthorizationUriAcquirer.cs
+++ Plugin.OAuth2.macOS/AuthorizationUriAcquirer.cs
@@ -1,18 +1,35 @@
 ﻿using Plugin.OAuth2.Common;
 using System.Threading.Tasks;
+using AppKit;
+using CoreGraphics;
 
 namespace Plugin.OAuth2.Components
 {
     internal class AuthorizationUriAcquirer : AuthorizationUriAcquirerBase
     {
+        private NSApplication Application => NSApplication.SharedApplication;
+
+        private NSWindow ModalWindow { get; set; }
+
         protected override Task ShowModalBrowserUI()
         {
+            ModalWindow = new NSWindow()
+            {
+                StyleMask = NSWindowStyle.Closable | NSWindowStyle.Titled
+            };
+
+            ModalWindow.WillClose += (sender, e) => CancellationHandler();
+
+            Application.RunModalForWindow(ModalWindow);
             return Task.CompletedTask;
         }
 
-        protected override Task CloseModalBrowserUI()
+        protected override async Task CloseModalBrowserUI()
         {
-            return Task.CompletedTask;
+            Application.AbortModal();
+
+            //Need to give the UI thread some time to recover
+            await Task.Delay(100);
         }
     }
 }
